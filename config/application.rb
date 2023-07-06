ea006f4774c35768ed1d73437243bfcb126db113
Bill Tihen <btihen@gmail.com>
2023-07-06 14:27:34 +0200

    modularize rails

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Modules
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.paths.add 'app/modules', glob: '*/{*,*/concerns}', eager_load: true
   end
 end
